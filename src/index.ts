2ee0eb39cb9f08c3a99d1a390a49fc6492f15ae9
Bruno Kelly <brunobuch00@gmail.com>
2026-02-08 20:28:43 -0300

    fixing index

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,32 @@
 import zustandPreferUseShallow from "./rules/zustand-prefer-use-shallow";
 import zustandRequireSelector from "./rules/zustand-require-selector";
 
-type RuleLevel = 0 | 1 | 2;
-type RuleEntry = RuleLevel | [RuleLevel, ...unknown[]];
-
-type FlatConfig = {
-  plugins?: Record<string, { rules: Record<string, unknown> }>;
-  rules?: Record<string, RuleEntry>;
-};
-
-type Plugin = {
-  rules: Record<string, unknown>;
-  configs?: Record<string, FlatConfig[]>;
-};
-
 export const rules = {
   "zustand-require-selector": zustandRequireSelector,
   "zustand-prefer-use-shallow": zustandPreferUseShallow,
-} satisfies Record<string, unknown>;
+};
 
-const pluginCore = {
+const plugin = {
   rules,
 };
 
-const recommended: FlatConfig[] = [
+const recommended = [
   {
     plugins: {
-      "state-management": pluginCore,
+      "state-management": plugin as any,
     },
     rules: {
       "state-management/zustand-require-selector": 2,
       "state-management/zustand-prefer-use-shallow": 1,
     },
   },
-];
+] as any[];
 
-const plugin: Plugin = {
-  ...pluginCore,
-  configs: {
-    recommended,
-  },
+export const configs = {
+  recommended,
 };
 
-export const configs = plugin.configs!;
-export default plugin;
+export default {
+  ...plugin,
+  configs,
+} as any;
